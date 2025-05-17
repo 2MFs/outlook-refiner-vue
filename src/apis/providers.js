a3f3959c3abce6d374fe05ad1efefa61777426c1
2MFs <loveminlife@gmail.com>
2025-05-17 23:34:25 +0800

    Fix provider error

--- src/apis/providers.js
+++ src/apis/providers.js
@@ -73,15 +73,9 @@ function createProvider(name, config = {}, env = import.meta.env, format = false
  */
 export function useProviders() {
   const configStore = useConfigStore();
-
-  const config = ref();
+  const config = computed(() => configStore.config || {});  
   const env = import.meta.env;
-
-  if (configStore.isLoaded) {
-    config = computed(() => configStore.config || {});  
-    // 可放心使用 config
-  }
-
+  
   return {
     openai: createProvider('openai', config, env),
     grok: createProvider('grok', config, env),
